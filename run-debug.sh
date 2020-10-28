1412cd86a26f9a94f89cdbf1141cde33ee7698dd
demon-rem <22884507+demon-rem@users.noreply.github.com>
2020-10-29 00:23:30 +0530

    Minor: Minor modifications to the project configurations
    Modify the configuration files related to the project - improves
    the process being followed to generate executables.
    
    The bash script is still botch-y at best, needs a near-total redo
    to be of some actual use, pushing it to the back-log for now.
    
    Update the cmake file with the recent changes being made to the
    project - cmake can be used to get the latest version of the project
    running directly.

--- run-debug.sh
+++ run-debug.sh
@@ -2,7 +2,7 @@
 # using the `-I` flag to add an additional search path for header files.
 #
 # Using the `lpcre` flag to use the library to access regex in the program.
-gcc encryptor.c -o "result_debug" -fms-extensions -I ./headers -lpcre
+gcc encryptor.c -o "result_debug" -fms-extensions -I /src/header -lpcre -I /src/implementations
 
 # Running the output file - also, passing any arguments passed to this bash file to the main file.
 ./"result_debug" $@

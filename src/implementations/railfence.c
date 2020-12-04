5e4b84bc61658ed983612ce2668761adf08ec3af
demon-rem <22884507+demon-rem@users.noreply.github.com>
2020-12-05 00:20:39 +0530

    Update: Finalize Railfence cipher
    Final commit for Railfence cipher, this commit finalizes the
    algorithm that is to be used in the case of Railfence cipher.

--- src/implementations/railfence.c
+++ src/implementations/railfence.c
@@ -96,6 +96,15 @@ extern inline unsigned int convert(string number) {
 	return result;
 }
 
+/**
+ * Calculates the length of the result string required in the matrix.
+ *
+ * @param str_len: Integer containing the length of the original string.
+ * @param rows: Integer containing the number of rows required in the matrix.
+ *
+ * @return
+ * 		Unsigned integer containing the length of the result string.
+ */
 extern inline unsigned int get_length(unsigned int str_len, unsigned int rows) {
 	unsigned int row = 0;
 	unsigned int column = 0;
@@ -109,7 +118,7 @@ extern inline unsigned int get_length(unsigned int str_len, unsigned int rows) {
 			dir_down = !dir_down;
 
 		new_len++;
-		if (new_len > str_len && row == rows - 1)
+		if (new_len >= str_len && row == rows - 1)
 			break;
 
 		column++;
@@ -176,8 +185,6 @@ string crypt_railfence(string key, string message, bool verbose) {
 	// Creating a 2d matrix which will be populated using characters of the message
 	char matrix[row_count][total_length];
 
-	printf("Message Length: %d\n", message_length);
-
 	for (unsigned int i = 0; i < row_count; i++)
 		for (unsigned int j = 0; j < total_length; j++)
 			matrix[i][j] = ' ';
@@ -208,7 +215,6 @@ string crypt_railfence(string key, string message, bool verbose) {
 		}
 	}
 
-
 	// Finally, fetching the result as needed.
 	unsigned int counter = 0;
 	for (unsigned int i = 0; i < row_count; i++)
@@ -220,6 +226,109 @@ string crypt_railfence(string key, string message, bool verbose) {
 	if (verbose)
 		printf("\n\n");
 
-	return
-		result;
+	return result;
+}
+
+string decrypt_railfence(string key, string message, bool verbose) {
+	// Checking if the key has been validate before. In case this fails, raising an error
+	// (Converting a logical bug into runtime error)
+	if (!rf_key_validated) {
+		printf("\nError: Attempt to encrypt a message without validating the key!\n");
+		exit(-10);
+	}
+
+	// Converting the string into an integer.
+	unsigned int row_count = convert(key);
+
+	// Calculating the current length of the string - and the extra length needed
+	// to pad the string such that it fits the diagonal.
+	unsigned int message_length = strlen(message);
+	unsigned int total_length = get_length(message_length, row_count);
+
+	// If the message is not of the required length, rejecting the input
+	if (message_length < total_length) {
+		printf("\n\nError: Invalid input detected. \n\n\tThe input string has "
+			   "incorrect padding \n\tAre you sure the input is correct?\n\n");
+
+		exit(-10);
+	}
+
+	if (verbose)
+		// Printing the padded version of the message.
+		printf("\nPadded message:\n\t%s\n\n", message);
+
+	// Create a new string to store the result into.
+	string result = (string) malloc(total_length * sizeof(char));
+
+	// Modifying each element of the result string to contain string terminator (hack-y approach)
+	for (unsigned int i = 0; i < total_length; i++)
+		result[i] = '\0';
+
+	// Creating a 2d matrix which will be populated using characters of the message
+	char matrix[row_count][total_length];
+
+	for (unsigned int i = 0; i < row_count; i++)
+		for (unsigned int j = 0; j < total_length; j++)
+			matrix[i][j] = ' ';
+
+	// Populating the 2d matrix.
+	bool dir_down = false;
+	unsigned int row = 0;
+	unsigned int column = 0;
+
+	int current_row = -1;
+
+	// First pass; marking the places with a star (*) symbol. These symbols will be
+	// replaced by characters in the next pass.
+	for (unsigned int i = 0; i < total_length; i++) {
+		if (row == 0 || row == row_count - 1)
+			dir_down = !dir_down;
+
+		if (row == 0)
+			current_row++;
+
+		matrix[row][column++] = '*';
+		dir_down ? row++ : row--;
+	}
+
+	// Second pass; replacing the star symbols from the first-pass with characters from
+	// the incoming message string.
+	unsigned int counter = 0;
+	for (unsigned int i = 0; i < row_count; i++) {
+		for (unsigned int j = 0; j < message_length; j++) {
+			if (matrix[i][j] == '*')
+				matrix[i][j] = message[counter++];
+		}
+	}
+
+	// Printing the matrix in verbose mode.
+	if (verbose) {
+		printf("\n\nMatrix: \n\n");
+
+		for (
+			unsigned int i = 0; i < row_count; i++) {
+			printf("%c\t", (i == 0) ? '\0' : '\n');
+
+			for (unsigned int j = 0; j < total_length; j++)
+				printf("%c\t", matrix[i][j]);
+		}
+	}
+
+	// Finally, fetching the result as needed.
+	row = 0;
+	column = 0;
+	for (unsigned int i = 0; i < total_length; i++) {
+		if (row == 0 || row == row_count - 1)
+			dir_down = !dir_down;
+
+		// Using the character at the current position if not a space.
+		if (matrix[row][column] != ' ')
+			result[i] = matrix[row][column++];
+		dir_down ? row++ : row--;
+	}
+
+	if (verbose)
+		printf("\n\n");
+
+	return result;
 }
